591a8749d4dbdccd1074e48b107147ed34962a69
Alexander Lenail <zfrenchee@gmail.com>
2017-02-17 08:07:48 -0500

    Wrote a draft of setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='OmicsIntegrator',
+    version='0.2.0',
+    packages=['OmicsIntegrator'],
+    package_dir={'OmicsIntegrator2': 'src'},
+    scripts=['src/forest.py'],
+    url='https://github.com/fraenkel-lab/OmicsIntegrator2',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'],
+    license='GNU General Public License',
+    author='zfrenchee',
+    author_email='alex@lenail.org',
+    description='',
+    install_requires=[
+        "numpy",
+        "pandas",
+        "garnet",
+        "networkx"
+    ],
+)
