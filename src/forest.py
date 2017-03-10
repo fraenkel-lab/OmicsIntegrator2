fc37385787f8d83827ccb636807d26f52d0dd6d3
Alexander Lenail <zfrenchee@gmail.com>
2017-03-10 16:03:39 -0500

    A lot of debugging

--- src/forest.py
+++ src/forest.py
@@ -97,27 +97,6 @@ params.add_argument("-s", "--seed", dest='seed', type=int, required=False,
 
 
 
-if __name__ == '__main__':
-
-	args = parser.parse_args()
-
-	params = vars(args.params) # http://stackoverflow.com/questions/42400646/is-it-possible-to-denote-some-set-of-argparses-arguments-without-using-subparse
-
-	graph = Graph(args.edge_file, params)
-
-	prizes, terminals = graph.prepare_prizes(args.prize_file)
-
-	if noisy_edges_repetitions + random_terminals_repetitions > 0:
-		nxgraph = graph.randomizations(noisy_edges_repetitions, random_terminals_repetitions)
-
-	else:
-		vertices, edges = graph.pcsf(prizes)
-		nxgraph = graph.output_forest_as_networkx(vertices, edges)
-
-	output_networkx_graph_as_gml_for_cytoscape(nxgraph, output_dir+'/output.gml')
-
-
-
 class Graph:
 	"""
 
@@ -144,7 +123,7 @@ class Graph:
 
 		interactome_fieldnames = ["source","target","cost"]
 		self.interactome_dataframe = pd.read_csv(interactome_file, delimiter='\t', names=interactome_fieldnames)
-		# self.interactome_graph = nx.from_pandas_dataframe(self.interactome_dataframe, 'source', 'target', edge_attr=['cost'])
+		self.interactome_graph = nx.from_pandas_dataframe(self.interactome_dataframe, 'source', 'target', edge_attr=['cost'])
 
 		# We first take only the source and target columns from the interactome dataframe.
 		# We then unstack them, which, unintuitively, stacks them into one column, which is a hack
@@ -187,6 +166,8 @@ class Graph:
 		"""
 
 		prizes_dataframe = pd.read_csv(prize_file, delimiter='\t', names=["name","prize"])
+		# Strangely some files have duplicates. We quietly keep the first value.
+		prizes_dataframe.drop_duplicates(subset=['name'], inplace=True)
 
 		# Here's we're indexing the terminal nodes and associated prizes by the indices we used for nodes
 		prizes_dataframe.set_index(self.nodes.get_indexer(prizes_dataframe['name']), inplace=True)
@@ -200,12 +181,8 @@ class Graph:
 		terminals = sorted(prizes_dataframe.index.values)
 
 		# Here we're making a dataframe with all the nodes as keys and the prizes from above or 0
-		prizes_dataframe = prizes_dataframe.merge(pd.DataFrame(self.nodes, columns=["name"]), on="name", how="outer").fillna(0)
-		# We re-index again, making sure we have a consistent index scheme
-		prizes_dataframe.set_index(self.nodes.get_indexer(prizes_dataframe['name']), inplace=True)
-		prizes_dataframe.sort_index(inplace=True)
-
-		# Our return value is a list, where each entry is a node's prize, indexed as above
+		prizes_dataframe = pd.DataFrame(self.nodes, columns=["name"]).merge(prizes_dataframe, on="name", how="left").fillna(0)
+		# Our return value is a 1D array, where each entry is a node's prize, indexed as above
 		prizes = prizes_dataframe['prize'].values
 
 		return prizes, terminals
@@ -214,10 +191,11 @@ class Graph:
 	def _add_dummy_node(self, connected_to=[]):
 
 		dummy_id = len(self.nodes)
+		dummy_prize = np.array([0])
 		dummy_edges = np.array([(dummy_id, connection) for connection in connected_to])
 		dummy_costs = np.array([self.params.w] * len(dummy_edges))
 
-		return dummy_edges, dummy_costs, dummy_id
+		return dummy_edges, dummy_costs, dummy_id, dummy_prize
 
 
 	def _check_validity_of_instance(self):
@@ -240,13 +218,14 @@ class Graph:
 		elif self.params.dummy_mode == 'all': endpoints = all
 		else: sys.exit("Invalid dummy mode")
 
-		dummy_edges, dummy_costs, root = self._add_dummy_node(connected_to=endpoints)
+		dummy_edges, dummy_costs, root, dummy_prize = self._add_dummy_node(connected_to=endpoints)
 
 		self._check_validity_of_instance()
 
 		# `edges`: a 2D int64 array. Each row (of length 2) specifies an undirected edge in the input graph. The nodes are labeled 0 to n-1, where n is the number of nodes.
 		edges = np.concatenate((self.edges, dummy_edges))
 		# `prizes`: the node prizes as a 1D float64 array.
+		prizes = np.concatenate((prizes, dummy_prize))
 		# `costs`: the edge costs as a 1D float64 array.
 		costs = np.concatenate((self.costs, dummy_costs))
 		# `root`: the root note for rooted PCST. For the unrooted variant, this parameter should be -1.
@@ -271,7 +250,7 @@ class Graph:
 			np.ndarray: edge weights with gaussian noise
 		"""
 
-		return np.clip(np.random.normal(self.costs, self.params.noise), 0, 1)
+		return np.clip(np.random.normal(self.costs, self.params.noise), 0.0001, 1)
 
 
 	def _random_terminals(self, prizes, terminals):
@@ -293,8 +272,8 @@ class Graph:
 
 		nodes_sorted_by_degree = pd.Series(self.node_degrees).sort_values().index
 		terminal_degree_rankings = np.array([nodes_sorted_by_degree.get_loc(terminal) for terminal in terminals])
-		new_terminal_degree_rankings = np.clip(terminal_degree_rankings + numpy.random.normal(0,100.0,len(terminals)), 0, len(self.nodes)).tolist()
-		new_terminals = pd.Series(nodes_sorted_by_degree)[new_terminal_degree_rankings][0]
+		new_terminal_degree_rankings = np.clip(np.rint(np.random.normal(terminal_degree_rankings, 10)), 0, len(self.nodes))
+		new_terminals = pd.Series(nodes_sorted_by_degree)[new_terminal_degree_rankings].values
 
 		new_prizes = copy(prizes)
 
@@ -305,7 +284,7 @@ class Graph:
 		return new_prizes, np.unique(new_terminals)
 
 
-	def randomizations(self, prizes, noisy_edges_reps, random_terminals_reps):
+	def randomizations(self, prizes, terminals, noisy_edges_reps, random_terminals_reps):
 		"""
 
 		Arguments:
@@ -333,8 +312,7 @@ class Graph:
 		self.costs = edge_costs
 
 		#### RANDOM TERMINALS ####
-		for random_prizes, terminals in [self._random_terminals(prizes) for rep in range(random_terminals_reps)]:
-
+		for random_prizes, terminals in [self._random_terminals(prizes, terminals) for rep in range(random_terminals_reps)]:
 			results.append(self.pcsf(random_prizes))
 
 		denominator = len(results)
@@ -427,4 +405,28 @@ def merge_two_prize_files(prize_file_1, prize_file_2):
 
 
 
+
+
+
+if __name__ == '__main__':
+
+	args = parser.parse_args()
+
+	# params = vars(args.params) # http://stackoverflow.com/questions/42400646/is-it-possible-to-denote-some-set-of-argparses-arguments-without-using-subparse
+	params = {}
+
+	graph = Graph(args.edge_file, params)
+
+	prizes, terminals = graph.prepare_prizes(args.prize_file)
+
+	if args.noisy_edges_repetitions + args.random_terminals_repetitions > 0:
+		nxgraph = graph.randomizations(prizes, terminals, args.noisy_edges_repetitions, args.random_terminals_repetitions)
+
+	else:
+		vertices, edges = graph.pcsf(prizes)
+		nxgraph = graph.output_forest_as_networkx(vertices, edges)
+
+	print(nxgraph)
+
+	# output_networkx_graph_as_gml_for_cytoscape(nxgraph, output_dir+'/output.gml')
 
