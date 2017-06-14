55629aa59138569a121b34ea2c05517749b4258b
Amanda Kedaigle <mandyjoy@mit.edu>
2017-06-14 16:29:04 -0400

    Name user saved JSON output the same
    So web tool page creation is simpler

--- visualization/src/run_visualization_for_user_saved_JSON_file.sh
+++ visualization/src/run_visualization_for_user_saved_JSON_file.sh
@@ -5,7 +5,7 @@
 #
 USER_SAVED_JSON_DIR=$1
 USER_SAMPLE=$2
-USER_RESULT_DIR="$USER_SAVED_JSON_DIR/visualize_saved_JSON_$USER_SAMPLE"
+USER_RESULT_DIR="$USER_SAVED_JSON_DIR/visualize_results_$USER_SAMPLE"
 mkdir $USER_RESULT_DIR
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #
@@ -27,5 +27,5 @@ python $SCRIPT_DIR/create_style_code_for_visualization.py $USER_RESULT_DIR $USER
 
 # Make the HTML file that contains JavaScript code using the cytoscape.js library to be user-specific in that directory,
 # so the HTML can be linked to a domain name and the user can access the weblink for their result
-cp $SCRIPT_DIR/visualize_user_saved_JSON_file_with_CytoscapeJS_lib.htm $USER_RESULT_DIR/visualize_user_saved_JSON_file_with_CytoscapeJS_lib_$USER_SAMPLE.htm
+cp $SCRIPT_DIR/visualize_user_saved_JSON_file_with_CytoscapeJS_lib.htm $USER_RESULT_DIR/visualize_OmicsIntegrator_results_with_CytoscapeJS_lib_$USER_SAMPLE.htm
 
