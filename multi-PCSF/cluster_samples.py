1b930129029454d2ba47f24c5221238dc6225009
Amanda Kedaigle <mandyjoy@mit.edu>
2017-08-11 17:05:14 -0400

    Dummy code for clustering

--- multi-PCSF/cluster_samples.py
+++ multi-PCSF/cluster_samples.py
@@ -4,3 +4,31 @@
 #   -Hierarchical clustering of omics data
 #   -Clustering of individual PCSF runs dynamically 
 #   -Pre-determined subtyping
+
+
+
+#Including some dummy code for an approximation of method 1 - clustering data
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+import numpy as np
+
+#random data for now
+a = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[100,])
+b = np.random.multivariate_normal([0, 20], [[3, 1], [1, 4]], size=[50,])
+X = np.concatenate((a, b),)
+
+Z = linkage(X, 'ward') #returns an array of length n - 1, Z[i] will tell us which clusters were merged in the i-th iteration
+
+# calculate full dendrogram
+plt.figure(figsize=(25, 10))
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('sample index')
+plt.ylabel('distance')
+dendrogram(
+    Z,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+plt.show()
+
+return Z
