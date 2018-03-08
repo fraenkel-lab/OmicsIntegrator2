cb292a45e6aab306e7f6c3bb8199e3b4716e8bf0
AmandaKedaigle <mandyjoy@mit.edu>
2018-03-07 23:58:55 -0500

    Multi PCSF (#66)
    * Set up multi-pcsf branch
    
    * Update cluster_samples.py
    
    * Dummy code for clustering
    
    * Add param screen code
    
    * Outline of multi_pcsf.py
    
    * Remove viz from param screen
    
    * Move param screen file
    
    * Notes for param screen
    
    * Corrections to outline
    
    * Update cluster_samples.py
    
    * Update cluster_samples.py
    
    * Update cluster_samples.py
    
    * Update cluster_samples.py
    
    * Update cluster_samples.py
    
    * Fleshing out some code
    
    * nodeFrequency()
    
    * Adjustments to get it to compile
    
    * Write new prizes to file
    
    * testing
    
    * calc_original_samples recursive function
    
    * Version that runs!
    
    * Toy example for testing code
    
    * Only add artificial prizes for Steiner nodes
    
    * Add alpha and lambda parameters
    
    * Details on params
    
    * single pcsf alpha to gamma
    
    * Updates to param screen
    
    (Include filename in output for multi setting)
    
    * Add output format as edge list
    
    * Read paths to prize files from a single file
    
    * Bug in write_graph_as_edgelist
    
    * Get rid of 'dir already exists' error
    
    * Param screen a -> g
    
    * missed one
    
    * Save memory by reading artificial prizes from file instead of dict
    
    * Name folders after initial prize files
    
    * changed the disimilarity to similarity
    
    * tab formatting
    
    * Add minClade param
    
    * MAJOR BUG FIX: keep track of last time we saw each sample
    
    * MAJOR BUG FIX: Set d_c to zero
    
    Previous behavior of similarity parameters led to unintuitive
    artificial prize changes.
    
    * Allow node attributes in prize files
    
    * Fix py2cytoscape calls
    
    * encoder for json
    
    * Remove param screen code for PR
    
    * use more common distance -> similarity metric
    
    * Add multiPCSF README

---
+++ multi-PCSF/run_multi_pcsf.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+
+#Run dendrogram-based multi-sample PCSF
+#constraining similar samples to result in similar networks
+
+#Inputs: 
+#   -prize file per sample
+#   -One interactome
+#   -Dendrogram representing heirarchical clustering of samples
+
+import argparse
+import sys, os
+import pickle
+from graph import Graph, output_networkx_graph_as_json_for_cytoscapejs
+
+def run_single_PCSF(prizeFile, edgeFile, paramDict, outdir):
+    #One straightforward application of PCSF
+    graph = Graph(edgeFile, paramDict)
+    graph.prepare_prizes(prizeFile)
+    vertex_indices, edge_indices = graph.pcsf()
+    forest, augmented_forest = graph.output_forest_as_networkx(vertex_indices, edge_indices)
+    output_networkx_graph_as_json_for_cytoscapejs(augmented_forest, outdir)
+    return forest
+
+def run_multi_PCSF(dendrogram, prizefileslist, edgeFile, minClade, paramDict, alpha, lbda, outdir):
+    #Iterate through dendrogram, and at each clade, run forest for each sample, adding artificial prizes
+    
+    dendrogram = pickle.load(open(dendrogram,'rb'))
+    with open(prizefileslist, 'r') as pf:
+        prizefiles = pf.readlines()
+    N = len(prizefiles)
+    lastF = [[] for _ in range(N)] #list of N lists of nodes in latest version of each sample
+    last_iteration_for_samples = [-1 for _ in range(N)] #keep track of last iteration where this sample appeared
+    origP = [[] for _ in range(N)] #keep track of original prizes per sample so we know which are Steiners
+
+    #Run the first iteration with unaltered prizes and note which nodes are terminals
+    os.makedirs(outdir + '/initial', exist_ok=True)
+    names = []
+    for i,p in enumerate(prizefiles):
+        p = p.strip()
+        p_name = os.path.basename(p)
+        names.append(p_name)
+        i_outdir = outdir + '/initial/%s'%p_name
+        os.makedirs(i_outdir, exist_ok=True)
+        unadjusted_forest = run_single_PCSF(p, edgeFile, paramDict, i_outdir)
+        lastF[i] = unadjusted_forest.nodes()
+        with open(p,'r') as pf:
+            for line in pf:
+                prot = line.split('\t')[0]
+                origP[i].append(prot)
+    
+    # Change the heights (dissimilarity) to similarity score: 
+    dist=[]               
+    for i, c in enumerate(dendrogram):
+         dist.append(float(c[2]))
+       
+    Similarity=[]             
+
+    for i in range(0,len(dist)):
+        Similarity.append(1/(1+dist[1])) #Need to account for all distances being the same
+
+    #now interate over dendrogram, and at each merge, re-run PCSF for samples in that merge
+    for i,c in enumerate(dendrogram):
+        num_samples_in_clade = int(c[3])
+        if num_samples_in_clade >= minClade:
+            os.makedirs(outdir + '/iter%i'%i, exist_ok=True)
+            s_c = float(Similarity[i])
+            sample1 = int(c[0])
+            sample2 = int(c[1])
+            samples_in_clade = calc_original_samples(sample1, N, dendrogram) + calc_original_samples(sample2, N, dendrogram)
+            if len(samples_in_clade) != num_samples_in_clade: sys.exit('Calculation of samples in clade %i went wrong'%i)
+        
+            #get frequency of nodes in these networks
+            forestFreq = nodeFrequency([lastF[k] for k in samples_in_clade])
+
+            #Update forests for each sample
+            for s in samples_in_clade:
+                s_outdir = outdir + '/iter%i/%s'%(i,names[s])
+                os.makedirs(s_outdir, exist_ok=True)
+                #read in artificial prizes we already assigned to this sample in previous iterations
+                last_iter = last_iteration_for_samples[s]
+                if last_iter == -1:
+                    artificial_prizes = {}
+                else:
+                    s_lastdir = outdir + '/iter%i/%s'%(last_iter,names[s])
+                    with open('%s/artificial_prizes.txt'%(s_lastdir), 'r') as a:
+                        artificial_prizes = {}
+                        for line in a:
+                            node, prize = line.strip().split('\t')
+                            artificial_prizes[node] = float(prize)
+                #Calculate new artificial prizes for this iteration
+                for node in forestFreq:
+                    if node not in origP[s]:
+                        if node in artificial_prizes:
+                            artificial_prizes[node] = artificial_prizes[node] + lbda*((s_c*forestFreq[node])**alpha) 
+                        else:
+                            artificial_prizes[node] = lbda*((s_c*forestFreq[node])**alpha)
+                #write new prizes + original prizes to a file
+                with open('%s/updated_prizes.txt'%(s_outdir),"w") as f:
+                    with open(prizefiles[s].strip(), "r") as p:
+                        origprizes = p.readlines()
+                        nCol = len(origprizes[1].split('\t'))
+                        if origprizes[-1][-1] != '\n': 
+                            origprizes[-1] = origprizes[-1] + '\n'
+                        f.writelines(origprizes)
+                    with open('%s/artificial_prizes.txt'%(s_outdir),'w') as a:
+                        for item in artificial_prizes:
+                            f.write("%s\t%s\t"%(str(item), str(artificial_prizes[item])) + '\t'.join(['0'] * (nCol-2)) + "\n" )
+                            a.write("%s\t%s\n" % (str(item), str(artificial_prizes[item])))
+                last_iteration_for_samples[s] = i
+
+                #submit new artificial prizes + orig prize list to run_single_pcsf
+                new_forest = run_single_PCSF('%s/updated_prizes.txt'%(s_outdir), edgeFile, paramDict, s_outdir)
+
+                #update lastF
+                lastF[s] = new_forest.nodes()
+            
+def calc_original_samples(sample, N, Z):
+    #recursively find all original samples in this merge
+    #if the sample number is greater than N, this is a clade of several other samples
+    if sample < N:
+        return [sample]
+    else:
+        newIdx = sample-N
+        return calc_original_samples(int(Z[newIdx][0]), N, Z) + calc_original_samples(int(Z[newIdx][1]), N, Z)
+        
+def nodeFrequency(list_of_node_lists):
+    #return dict with node:freq of all nodes in these lists
+    n = float(len(list_of_node_lists)) #want floating point division
+    countDict = {}
+    for forest in list_of_node_lists:
+        for node in forest:
+            if node in countDict:
+                countDict[node] += 1
+            else:
+                countDict[node] = 1
+    freqDict = {}
+    for node in countDict:
+        freqDict[node] = countDict[node] / n
+    return freqDict
+        
+
+def main():
+    parser = argparse.ArgumentParser(description="""
+	Run the PCSF algorithm several times on hierarchically clustered samples, refining the tree for each sample so that similar
+    samples result in similar trees.""")
+
+    parser.add_argument("-e", "--edge", dest='edge_file', required=True,
+	    help ='(Required) Path to the text file containing the edges. Should be a tab delimited file with 3 columns: "nodeA\tnodeB\tcost"')
+    parser.add_argument("-p", "--prizefiles", dest='prize_files', required=True,
+	    help='(Required, one or more) A text file containing paths to the files containing the prizes. The list should be in the same order as was provided to create the dendrogram. Each path should lead to a tab delimited files with lines: "nodeName(tab)prize"')
+    parser.add_argument("-d", "--dendrogram", dest="dendrogram", required=True,
+        help='(Required) pickled object denoting hierarchical clustering of samples, of the type returned by scipy.cluster.heirarchy\'s linkage().. Should be an array of length n-1, where dendrogram[i] indicates which clusters are merged at the i-th iteration.')
+    parser.add_argument('-o', '--output', dest='output_dir', default='.',
+	    help='Output directory path. Default current directory')
+    parser.add_argument('-m', '--minClade', dest='minClade', default=2, type=int,
+	    help='Only calculate artificial prizes for clades that contain at least m samples. Default = all clades')
+
+    parser.add_argument("-w", dest="w", default=5, type=float, help="Omega: the weight of the edges connecting the dummy node to the nodes selected by dummyMode [default: 5]")
+    parser.add_argument("-b", dest="b", default=1, type=float, help="Beta: scaling factor of prizes [default: 1]")
+    parser.add_argument("-g", dest="g", default=0, type=float, help="Gamma: Edge penalty on hubs [default: 0]")
+    parser.add_argument("-l", "--lambda",dest="lbda", default=1, type=float, help="Lambda: scaling factor on artificial prizes [default: 1]")
+    parser.add_argument("-a", "--alpha",dest="alpha", default=0, type=float, help="Alpha: non-linear scaling on artificial prizes [default: 0]")
+    
+    args = parser.parse_args()
+    paramDict = {'w':args.w, 'b':args.b, 'g':args.g}
+
+    run_multi_PCSF(args.dendrogram, args.prize_files, args.edge_file, args.minClade, paramDict, args.alpha, args.lbda, args.output_dir)
+
+
+if __name__ == '__main__':
+	main()
