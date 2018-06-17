c1bf757bd60f3c4cbe34c0a0cba6f88f99d044eb
Alexander Lenail <zfrenchee@gmail.com>
2018-06-17 18:15:41 -0400

    just a few renames

--- src/graph.py
+++ src/graph.py
@@ -647,9 +647,9 @@ def annotate_graph_nodes(nxgraph):
     """
 
     try:
-        annotation = pd.read_pickle(get_path('OmicsIntegrator', 'annotation/subcellular.pickle'))
+        annotation = pd.read_pickle(get_path('OmicsIntegrator', 'annotation/final_annotation.pickle'))
     except:
-        annotation = pd.read_pickle(Path.cwd().parent / 'annotation' / 'subcellular.pickle')
+        annotation = pd.read_pickle(Path.cwd().parent / 'annotation' / 'final_annotation.pickle')
 
     nx.set_node_attributes(nxgraph, annotation.reindex(list(nxgraph.nodes())).dropna(how='all').to_dict(orient='index'))
 
