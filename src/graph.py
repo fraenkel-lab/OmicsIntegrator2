2305c24c3ad9a0350ae6587b76d0706f84b0b224
Alexander Lenail <zfrenchee@gmail.com>
2018-06-17 18:39:10 -0400

    Fix stuff, run tests, run example_usage notebook

--- src/graph.py
+++ src/graph.py
@@ -115,12 +115,12 @@ class Graph:
             params (dict): params with which to run the program
         """
 
-        defaults = {"w": 5, "b": 1, "g": 3, "edge_noise": 0.1, "dummy_mode": "terminals", "seed": None, "skip_checks": False}
+        defaults = {"w": 5, "b": 1, "g": 3, "edge_noise": 0.1, "dummy_mode": "terminals", "seed": 0, "skip_checks": False}
 
         # Overwrite the defaults with any user-specified parameters.
         self.params = Options({**defaults, **params})
 
-        if not self.params.skip_checks: _check_validity_of_hyperparameters()
+        if not self.params.skip_checks: self._check_validity_of_hyperparameters()
 
         # Add costs to each edge, proportional to the degrees of the nodes it connects, modulated by parameter g.
         N = len(self.nodes)
@@ -139,13 +139,12 @@ class Graph:
         Assert that the hyperparameters passed to this program are valid, otherwise raise helpful error messages.
         """
 
-        if not (isinstance(self.params.w, numbers.Number) && (self.params.w >= 0)): raise ValueError("parameter w must be a positive number. Was "+str(self.params.w))
-        if not (isinstance(self.params.b, numbers.Number) && (self.params.b >= 0)): raise ValueError("parameter b must be a positive number. Was "+str(self.params.b))
-        if not (isinstance(self.params.g, numbers.Number) && (self.params.g >= 0)): raise ValueError("parameter g must be a positive number. Was "+str(self.params.g))
-        if not (isinstance(self.params.edge_noise, numbers.Number) && (self.params.edge_noise >= 0)): raise ValueError("parameter edge_noise must be a positive number. Was "+str(self.params.edge_noise))
+        if not (isinstance(self.params.w, numbers.Number) and (self.params.w >= 0)): raise ValueError("parameter w must be a positive number. Was "+str(self.params.w))
+        if not (isinstance(self.params.b, numbers.Number) and (self.params.b >= 0)): raise ValueError("parameter b must be a positive number. Was "+str(self.params.b))
+        if not (isinstance(self.params.g, numbers.Number) and (self.params.g >= 0)): raise ValueError("parameter g must be a positive number. Was "+str(self.params.g))
+        if not (isinstance(self.params.edge_noise, numbers.Number) and (self.params.edge_noise >= 0)): raise ValueError("parameter edge_noise must be a positive number. Was "+str(self.params.edge_noise))
         if not (self.params.dummy_mode in ['terminals', 'other', 'all']): raise ValueError("parameter dummy_mode must be one of 'terminals', 'other', or 'all'. Was "+str(self.params.dummy_mode))
         if not (isinstance(self.params.seed, int)): raise ValueError("parameter seed must be a int. Was type "+str(type(self.params.seed)))
-        if not (isinstance(self.params.skip_checks, bool)): raise ValueError("parameter skip_checks must be a bool. Was type "+str(type(self.params.skip_checks)))
 
 
     def prepare_prizes(self, prize_file):
