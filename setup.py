05f3d98eb50f986a43f407e7ba7611b1541fe37e
Alexander Lenail <zfrenchee@gmail.com>
2018-06-20 20:48:40 -0400

    clean up docs & update sphinx.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=['OmicsIntegrator'],
     package_dir={'OmicsIntegrator': 'src'},
     package_data={'OmicsIntegrator': ['annotation/final_annotation.pickle', 'src/viz.jinja', 'viz.jinja']},
-    version='2.1.0',
+    version='2.2.0',
     url='https://github.com/fraenkel-lab/OmicsIntegrator2',
     classifiers=[
         'Programming Language :: Python :: 3.5',
